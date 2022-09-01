2eace302105529562a1935446613f221b77e82b3
지은 <107034832+6868x88@users.noreply.github.com>
2022-09-01 15:57:19 +0900

    selectboard(), insertboard()

--- Web-study-11/src/main/java/com/saeyan/dao/BoardDAO.java
+++ Web-study-11/src/main/java/com/saeyan/dao/BoardDAO.java
@@ -1,6 +1,7 @@
 package com.saeyan.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -21,21 +22,22 @@ public class BoardDAO {
 	}
 
 
+	//DB목록 불러오기
 	public List<BoardVO> selectAllBoards(){
-		String sql = "select * from board order by num desc";
+		String sql = "select * from bboard order by num desc";
 
 		List<BoardVO> list = new ArrayList<BoardVO>();
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-		
+
 
 		try {
 			conn = DBManager.getConnection();
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(sql);
-			
-			
+
+
 			while(rs.next()) {
 				BoardVO vo = new BoardVO();
 				vo.setNum(rs.getInt("num"));
@@ -49,10 +51,40 @@ public class BoardDAO {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			DBManager.close(conn, stmt, rs);
 		}
-		
+
 		return list;
 	}
+
+	//DB입력
+	public void insertBoard(BoardVO vo) {
+		String sql = "insert into bboard("
+				+ "num, name, email, pass, title, content) "
+				+ "values(board_seq.NEXTVAL, ?, ? ,? ,? ,?)";
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getEmail());
+			pstmt.setString(3, vo.getPass());
+			pstmt.setString(4, vo.getTitle());
+			pstmt.setString(5, vo.getContent());
+
+			pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+	}
+	
+	
 }
